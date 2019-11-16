4af76954355c783fd0ad2ba1f70449a92442670e
feriteja <feriteja@gmail.com>
2019-11-16 12:18:57 +0700

    nomber 2

--- 2.js
+++ 2.js
@@ -1,7 +1,12 @@
 username = "";
 password = "";
 
-function login(username, password) {
+function validate(username, password) {
+  let ceckPassword = new password();
+
+  console.log(ceckPassword);
+
   if (username.length == 7 && username == username.toUpperCase()) {
+    console.log("ok 7");
   }
 }
